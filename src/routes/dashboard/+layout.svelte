15687341c332c7a8514df9a923b8127b49252d18
Henry Bui <henry.h.bui05@gmail.com>
2024-07-25 23:14:58 +0200

    use dashboard/+layout.svelte instead of home-rolled router

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <a href="/dashboard/roll-call">Roll Call</a>
+    <a href="/dashboard/moderated-caucus">Moderated Caucus</a>
+    <a href="/dashboard/speaker-list">Speakers List</a>
+    <a href="/dashboard/points-motions">Points and Motions</a>
+</nav>
+
+<slot></slot>
