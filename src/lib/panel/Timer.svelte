86cfebeb2fb0070fe5bf00252a0722e0d499f368
Henry Bui <henry.h.bui05@gmail.com>
2024-07-26 15:34:06 +0200

    timer!

---
+++ src/lib/panel/Timer.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+    export let duration: number;
+
+    const DURATION_MS = duration * 1000;
+
+    let msRemaining = DURATION_MS;
+    let lastTs: number;
+    let callId: number;
+    let running: boolean = false;
+
+    let bar: HTMLDivElement;
+    let color = "green";
+
+    export function start() {
+        lastTs = performance.now();
+        callId = requestAnimationFrame(timerLoop);
+    }
+    export function pause() {
+        cancelAnimationFrame(callId);
+        running = false;
+    }
+    export function reset() {
+        pause();
+        msRemaining = DURATION_MS;
+    }
+
+    function timerLoop() {
+        let currentTs = performance.now();
+        msRemaining -= Math.floor(currentTs - lastTs);
+        lastTs = currentTs;
+
+        // Continue animating if time is not up
+        running = msRemaining > 0;
+        if (running) {
+            callId = requestAnimationFrame(timerLoop);
+        }
+    }
+    function timeString(ms: number): string {
+        if (ms < 0) ms = 0;
+        let secsElapsed = Math.ceil(ms / 1000);
+
+        let sec = secsElapsed % 60;
+        let min = Math.floor(secsElapsed / 60);
+        let hr;
+        let day;
+
+        if (min >= 60) {
+            hr = Math.floor(min / 60);
+            min %= 60;
+
+            if (hr >= 24) {
+                day = Math.floor(hr / 24);
+                hr %= 24;
+            }
+        }
+        
+        return [day, hr, min, sec]
+            .filter(n => typeof n != "undefined")
+            .map(n => String(n).padStart(2, '0'))
+            .join(':');
+    }
+    function clamp(value: number, min: number, max: number) {
+        return Math.min(Math.max(min, value), max)
+    }
+
+    $: bar?.style.setProperty("--progress", String(clamp(msRemaining / DURATION_MS, 0, 1)));
+    $: {
+        if (msRemaining <= 0.2 * DURATION_MS) color = "red";
+        else if (msRemaining <= 0.5 * DURATION_MS) color = "yellow";
+        else color = "green";
+    }
+</script>
+<style>
+    .top {
+        display: flex;
+        flex-direction: column;
+
+        border: 1px solid black;
+    }
+    .bar {
+        width: calc(100% * var(--progress));
+        height: 20px;
+        background-color: green;
+        transition: background-color 1s;
+    }
+    .time {
+        align-self: center;
+    }
+
+    .bar.yellow {
+        background-color: goldenrod;
+    }
+    .bar.red {
+        background-color: red;
+    }
+    .bar:not(.running) {
+        /* If running, we don't want to use a width transition as that would interfere with the JS animation */
+        transition: background-color 1s, width 1s;
+    }
+</style>
+
+<div class="top">
+    <div class="bar" bind:this={bar} class:yellow={color === "yellow"} class:red={color === "red"} class:running></div>
+    <div class="time">{timeString(msRemaining)}/{timeString(DURATION_MS)}</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
     import "$lib/panel/public";
+    import Timer from "$lib/panel/Timer.svelte";
 
+    let timer: Timer;
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="/admin" target="_blank">/admin</a> to open admin panel</p>
+<button on:click={timer.start}>Start</button>
+<button on:click={timer.pause}>Pause</button>
+<button on:click={timer.reset}>Reset</button>
+<Timer duration={10} bind:this={timer} />
