bf024f847810989013359707ee31957dca3efb78
Henry Bui <henry.h.bui05@gmail.com>
2024-08-05 13:47:19 -0400

    skeleton ui + iconify scaffolding

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+    import { computePosition, autoUpdate, offset, shift, flip, arrow, size } from '@floating-ui/dom';
+    import { initializeStores, storePopup } from "@skeletonlabs/skeleton";
+
+    initializeStores();
+    storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow, size });
+</script>
+
+<slot></slot>
