eb2a839ada7917815154cdf37ee4cdd03caa5543
Henry Bui <henry.h.bui05@gmail.com>
2024-08-22 23:57:45 -0400

    front route

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+    import Icon from "@iconify/svelte";
+</script>
+
+<div class="flex flex-col justify-center h-screen gap-3">
+    <h1 class="h1 text-center">CouchMUN</h1>
+
+    <!-- Entry buttons -->
+    <div class="flex justify-center gap-3">
+        <a class="btn variant-filled-primary" href="{base}/dashboard">Open Dashboard</a>
+        <a class="btn-icon variant-filled-primary" href="{base}/settings">
+            <Icon icon="mdi:gear" width="24" height="24" />
+        </a>
+    </div>
+</div>
