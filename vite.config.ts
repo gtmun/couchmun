555ff9c82db041b8ce36af189c78cc5de91b74e8
Henry Bui <henry.h.bui05@gmail.com>
2024-09-21 00:04:16 -0400

    fix top level await issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	// Allow any usage of new JS features:
+	build: {
+		target: "esnext"
+	},
+	esbuild: {
+		target: "esnext"
+	},
+	optimizeDeps:{
+		esbuildOptions: {
+			target: "esnext",
+		}
+	},
+	
 	plugins: [sveltekit()]
 });
