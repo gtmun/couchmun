f745ed35ad3176c82dcc4b5dc9eb2a1dc7126e9e
Henry Bui <henry.h.bui05@gmail.com>
2024-09-22 23:42:39 -0400

    use radio groups instead of table for roll call (this one may be controversial)

--- src/routes/dashboard/roll-call/+page.svelte
+++ src/routes/dashboard/roll-call/+page.svelte
@@ -1,71 +1,25 @@
 <script lang="ts">
   import { getSessionDataContext } from "$lib/stores/session";
+  import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
 
   const { settings: { delegateAttributes }, delegateAttendance } = getSessionDataContext();
 </script>
 
 <!-- Render a table to display participants and their statuses -->
-<div class="table-container">
-  <table class="table table-compact">
-    <thead>
-      <tr>
-        <th>Delegate</th>
-        <th class="w-1/4">Present</th>
-        <th class="w-1/4">Present and Voting</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each Object.keys($delegateAttributes) as key}
-        {@const presence = $delegateAttendance[key] ??= "NP"}
-        <tr>
-          <td>{$delegateAttributes[key].name}</td>
-          <td 
-            class="data-cell"
-            class:selected={presence != "NP"}
-          >
-            <button 
-              class="w-full h-full"
-              on:click={() => $delegateAttendance[key] = presence === "NP" ? "P" : "NP"}
-            />
-          </td>
-          <td 
-            class="data-cell"
-            class:selected={presence === "PV"}
-          >
-            <button 
-              class="w-full h-full"
-              on:click={() => $delegateAttendance[key] = presence === "PV" ? "P" : "PV"}
-            />
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+<div class="card grid grid-cols-[1fr-1fr]">
+  {#each Object.keys($delegateAttributes) as key}
+    {@const _presence = $delegateAttendance[key] ??= "NP"}
+    <div class="grid grid-cols-subgrid col-span-2 even:bg-surface-100-800-token odd:bg-surface-200-700-token">
+      <div class="flex items-center p-4">
+          {$delegateAttributes[key].name}
+      </div>
+      <div class="flex flex-col justify-center p-2">
+        <RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary" border="" background="bg-surface-300-600-token">
+          <RadioItem bind:group={$delegateAttendance[key]} name="presence-{key}" value={"NP"}>Absent</RadioItem>
+          <RadioItem bind:group={$delegateAttendance[key]} name="presence-{key}" value={"P"}>Present</RadioItem>
+          <RadioItem bind:group={$delegateAttendance[key]} name="presence-{key}" value={"PV"}>Present and Voting</RadioItem>
+        </RadioGroup>
+      </div>
+    </div>
+  {/each}
 </div>
-
-<style>
-  td.data-cell {
-    border-left-width: 1px;
-    border-left-color: rgb(var(--color-surface-500) / 0.2);
-    transition: background-color 0.2s;
-    padding: 0 !important;
-    /**
-      Actually stupid, but whatever.
-      The attributes below allow h-full on button to apply,
-      allowing the button to expand the entire table cell.
-
-      It does not actually affect the height of the table.
-    */
-    height: 0;
-    min-height: 0;
-  }
-  td.data-cell.selected {
-    background-color: rgba(var(--color-primary-500));
-  }
-  td.data-cell:hover {
-    background-color: rgba(var(--color-primary-500) / 0.25);
-  }
-  td.data-cell.selected:hover {
-    background-color: rgba(var(--color-primary-500) / 0.75);
-  }
-</style>
