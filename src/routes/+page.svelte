983c6e0a6b2c7149861f5dcc10f5be7d7f9cc2ff
Henry Bui <henry.h.bui05@gmail.com>
2024-10-21 09:30:46 -0400

    add alpha dev warning on the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,30 @@
     description="A tool to support chairs of Model UN committees in keeping time, overseeing debate, and managing delegates."
 />
 
-<div class="flex flex-col justify-center h-screen gap-3">
-    <h1 class="h1 text-center">CouchMUN</h1>
-
-    <!-- Entry buttons -->
-    <div class="flex justify-center gap-3">
-        <a class="btn variant-filled-primary" href="{base}/dashboard">
-            Open Dashboard
-        </a>
-        <a 
-            class="btn-icon variant-filled-primary" 
-            href="{base}/admin/settings"
-            aria-label="Settings"
-            title="Settings"
-        >
-            <Icon icon="mdi:gear" width="24" height="24" />
-        </a>
+<div class="flex flex-col justify-between h-screen p-3">
+    <div></div>
+    <div class="flex flex-col justify-center h-screen gap-3">
+        <h1 class="h1 text-center">CouchMUN</h1>
+    
+        <!-- Entry buttons -->
+        <div class="flex justify-center gap-3">
+            <a class="btn variant-filled-primary" href="{base}/dashboard">
+                Open Dashboard
+            </a>
+            <a 
+                class="btn-icon variant-filled-primary" 
+                href="{base}/admin/settings"
+                aria-label="Settings"
+                title="Settings"
+            >
+                <Icon icon="mdi:gear" width="24" height="24" />
+            </a>
+        </div>
+    </div>
+    <div class="text-right">
+        <i>
+            CouchMUN is in its alpha development stages.<br>
+            If you catch any bugs, report them to <a class="anchor" href="https://github.com/gtmun/couchmun/">its repository</a>.
+        </i>
     </div>
 </div>
