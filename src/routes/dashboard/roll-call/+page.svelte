b1358c908bddb3fb4832abf96bfb0017f473f1c6
Henry Bui <henry.h.bui05@gmail.com>
2025-12-24 03:06:11 -0500

    Use preset-tonal in some applicable places

--- src/routes/dashboard/roll-call/+page.svelte
+++ src/routes/dashboard/roll-call/+page.svelte
@@ -82,7 +82,7 @@
             <h3 class="h3">No delegates enabled.</h3>
                 Visit 
                 <a
-                    class="btn btn-sm preset-filled-warning-100-900"
+                    class="btn btn-sm preset-tonal-warning"
                     href="{resolve("/admin/settings")}"
                     tabindex="0"
                 >
