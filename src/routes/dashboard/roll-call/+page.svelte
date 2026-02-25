0020330e59555be420f0f3749eed427972c61489
Henry Bui <henry.h.bui05@gmail.com>
2026-02-25 02:19:57 -0500

    Add search to roll-call

--- src/routes/dashboard/roll-call/+page.svelte
+++ src/routes/dashboard/roll-call/+page.svelte
@@ -4,13 +4,14 @@
 -->
 <script lang="ts">
     import { resolve } from "$app/paths";
-    import RollCall from "$lib/components/RollCall.svelte";
+    import RollCall, { notPendingThen } from "$lib/components/RollCall.svelte";
     import { getSessionContext } from "$lib/context/index.svelte";
     import { db } from "$lib/db/index.svelte";
     import type { DelegatePresence } from "$lib/types";
     import MdiAccount from "~icons/mdi/account";
     import MdiAccountCheck from "~icons/mdi/account-check";
     import MdiAccountOff from "~icons/mdi/account-off";
+    import MdiSearch from "~icons/mdi/search";
 
     const { delegates } = getSessionContext();
 
@@ -18,33 +19,44 @@
         if (s === "P" || s === "PV") return s;
         return "NP";
     }
+
+    let rcSearchQuery = $state("");
+    let rcDelegates = $derived(notPendingThen($delegates, dels => dels.filter(d => d.nameIncludes(rcSearchQuery))));
 </script>
 
-<!-- Render a table to display participants and their statuses -->
-<RollCall
-    getValue={(d) => d.presence}
-    setValue={(value, d) => db.updateDelegate(d.id, { presence: asPresence(value) })}
-    delegates={$delegates}
-    entries={[
-        { value: "NP", label: "Absent", icon: MdiAccountOff },
-        { value: "P",  label: "Present", icon: MdiAccount },
-        { value: "PV", label: "Present & Voting", icon: MdiAccountCheck },
-    ]}
->
-    {#snippet emptyPlaceholder()}
-        <div class="h-full w-full flex flex-col items-stretch justify-center">
-            <div class="text-center">
-                <h3 class="h3">No delegates enabled.</h3>
-                    Visit 
-                    <a
-                        class="btn btn-sm preset-tonal-warning"
-                        href="{resolve("/admin/settings")}"
-                        tabindex="0"
-                    >
-                        Settings
-                    </a>
-                to configure delegates.
-            </div>
-        </div>
-    {/snippet}
-</RollCall>
+<div class="flex flex-col h-full gap-4">
+    <div class="flex gap-2 items-center">
+        <MdiSearch />
+        <input class="input" bind:value={rcSearchQuery} placeholder="Search...">
+    </div>
+    <!-- Render a table to display participants and their statuses -->
+    <div class="flex overflow-hidden">
+        <RollCall
+            getValue={(d) => d.presence}
+            setValue={(value, d) => db.updateDelegate(d.id, { presence: asPresence(value) })}
+            delegates={rcDelegates}
+            entries={[
+                { value: "NP", label: "Absent", icon: MdiAccountOff },
+                { value: "P",  label: "Present", icon: MdiAccount },
+                { value: "PV", label: "Present & Voting", icon: MdiAccountCheck },
+            ]}
+        >
+            {#snippet emptyPlaceholder()}
+                <div class="h-full w-full flex flex-col items-stretch justify-center">
+                    <div class="text-center">
+                        <h3 class="h3">No delegates enabled.</h3>
+                            Visit 
+                            <a
+                                class="btn btn-sm preset-tonal-warning"
+                                href="{resolve("/admin/settings")}"
+                                tabindex="0"
+                            >
+                                Settings
+                            </a>
+                        to configure delegates.
+                    </div>
+                </div>
+            {/snippet}
+        </RollCall>
+    </div>
+</div>
